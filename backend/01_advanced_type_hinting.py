c83fcaba32fd88fb12e2aa454d17a2751f1ad160
vamsime <mymail.vamsi@gmail.com>
2024-02-07 12:00:06 +0530

    first commit

---
+++ backend/01_advanced_type_hinting.py
@@ -0,0 +1,8 @@
+from typing import List, Tuple, Dict
+
+price: List[int] = [10, 12, 14, 16.2]
+print(price)
+price: Tuple[int, int, int] = [10, 12, 14]
+print(price)
+price: Dict[str, int] = {'item1': 23, 'item2': 45}
+print(price)
