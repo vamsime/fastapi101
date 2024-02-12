ae2018b20abc763443fe7acfea926d7e27f19cc9
vamsime <mymail.vamsi@gmail.com>
2024-02-12 13:12:05 +0530

    created a list view of all the blogs; showing only the active ones - by looping through each of them in the HTML

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+from db.repository.blog import list_blogs
+from db.session import get_db
 
 
 # initialize an instance of the template
@@ -10,9 +13,10 @@ router = APIRouter()
 # if someone visits the home url, or the index url - FastAPI needs to serve this function;
 #  which is why we are only giving / as argument
 @router.get("/")
-def home(req: Request):
-    print(dir(req))
-    return templates.TemplateResponse("blogs/home.html", {"request": req})
+def home(req: Request, db_session: Session = Depends(get_db)):
+    blogs = list_blogs(db_session)  # fetch the list of all the active blogs
+    context_dictionary = {"request": req, 'blogs': blogs}
+    return templates.TemplateResponse("blogs/home.html", context_dictionary)
 # 1) The FastAPI sends a request to this router
 # 2) The home() gets executed which takes the request and parses it to home.html file
 # 3) home.html inherits the base.html
